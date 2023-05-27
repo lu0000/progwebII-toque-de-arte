20ce2446cddd6188add8aa12971e57e87883e3be
Emynath <134175301+Emynath@users.noreply.github.com>
2023-05-27 11:25:41 -0300

    adicionando parte dos arrays

---
+++ arrays.php
@@ -0,0 +1,166 @@
+<?php
+
+$destaque = [
+    [
+        "nome" => "Africana Pote",
+        "foto" => "imagens/destaques/africana-1-60x85cm-pintura-a-oleo.jpg",
+        "preco" => "R$ 420,00",
+        "descricao" => "Tamanho 1.60x85cm; São todas pinturas óleo sob tela e Pintadas a mão."
+    ],
+    [
+        "nome" => "Africana Mãe",
+        "foto" => "imagens/destaques/africana-mae-1-70x85cm-mulheres-africanas.jpg",
+        "preco" => "R$ 420,00",
+        "descricao" => "Tamanho 1.60x85cm; Pinturas óleo sob tela e Pintadas a mão.
+        Pintadas a mão"
+    ],
+    [
+        "nome" => "Africana Tamanho",
+        "foto" => "imagens/destaques/africana-tamanho-1-40x80cm-oleo-sob-tela-quadro.jpg",
+        "preco" => "R$ 380,00",
+        "descricao" => "Africana: Tamanho 1.40x80cm; Pintura óleo sobre tela e Pintados a mão"
+    ],
+    [
+        "nome" => "Lampião Pintura",
+        "foto" => "imagens/destaques/lampiao-pintura-1-60x85cm-abstrato-colorido.jpg",
+        "preco" => "R$ 420,00",
+        "descricao" => "Art' Lampião: Tamanho 1.60x85cm; São todas pinturas óleo sob tela e Pintadas a mão."
+    ],
+    [
+        "nome" => "Pintura em Tela Sertanejos Autoria Katiúcia Mafra",
+        "foto" => "imagens/destaques/pintura-em-tela-sertanejos-autoria-katiucia-mafra-decoracao-nordestina.jpg",
+        "preco" => "R$ 160,00",
+        "descricao" => "Pintura em tela tamanho 30 x20; Título: Romance no meu sertão; Tema: Sertanejos; Autoria : Katiúcia Mafra; Coleção no estilo Naif."
+    ],
+    [
+        "nome" => "Trio de forrozeiros Tamanho",
+        "foto" => "imagens/destaques/trio-de-forrozeiros-tamanho-1-60x85cm-pinturas-oleo-quadros-modernos.jpg",
+        "preco" => "R$ 420,00",
+        "descricao" => "Trio de forrozeiros; Tamanho 1.60x85cm; Pinturas óleo sobre; tela feitos a mão livre."
+    ]
+];
+
+$promocoes = [
+    [
+        "nome" => "Bolsa De Crochê Fio De Malha",
+        "foto" => "imagens/promocoes/bolsa-de-croche-fio-de-malha-croche.jpg",
+        "preco_antigo" => "R$ 260,00",
+        "preco_novo" => "R$ 210,00",
+        "descricao" => "Alças em crochê ou em correntes; Disponível corrente dourada. Acompanha Chaveiro e Com ou sem forro."
+    ],
+    [
+        "nome" => "Bolsa de crochê fio de malha",
+        "foto" => "imagens/promocoes/bolsa-de-croche-fio-de-malha-praiana (1).jpg",
+        "preco_antigo" => "R$ 289,90",
+        "preco_novo" => "R$ 262,00",
+        "descricao" => "A bolsa Picnic é uma bolsa casual de verão para lazer e passeios. O design da bolsa é complementado pela adição de uma peça redonda de madeira. A bolsa fica linda em cores naturais combinadas com a cor da madeira natural."
+    ],
+    [
+        "nome" => "Bolsa de crochê fio de malha",
+        "foto" => "imagens/promocoes/bolsa-de-croche-fio-de-malha-praiana.jpg",
+        "preco_antigo" => "R$ 280,90",
+        "preco_novo" => "R$ 264,90 ",
+        "descricao" => "Laguna é uma bolsa redonda grande, diámetro 32-34 cm, espessura 12-14 cm. Um modelo muito popular para o verão e fora de temporada. Dependendo da cor, a bolsa parece uma bolsa de escritório formal ou uma bolsa de praia."
+    ],
+    [
+        "nome" => "Baiana - Boneca Artesanal",
+        "foto" => "imagens/promocoes/boneca-artesanal-baiana-bonecabaiana-baiana-bonecanegra-bonecaartesanal.jpg",
+        "preco_antigo" => "R$ 200,00",
+        "preco_novo" => "R$ 130,50",
+        "descricao" => "Baiana - Boneca Artesanal: Material: base em jornal, decorado com tecido e metais. Acompanha caixa para presente branca com tampa transparente - acetato e fita."
+    ],
+    [
+        "nome" => "Kit Esquerda - Bonecos Artesanais",
+        "foto" => "imagens/promocoes/kit-esquerda-bonecos-artesanais-umbanda-bonecos-artesanais.jpg",
+        "preco_antigo" => "R$ 300,00",
+        "preco_novo" => "R$ 226,60",
+        "descricao" => "Kit Esquerda - contém: 1 Pomba Gira e 1 Exú; Material: base em jornal, decorado com tecido e acessórios. Acompanha caixa de presente branca com tampa transparente - acetato e fita."
+    ],
+    [
+        "nome" => "Zé Pelintra - Boneco Artesanal",
+        "foto" => "imagens/promocoes/ze-pelintra-boneco-artesanal-orixas-boneco-artesanal.jpg",
+        "preco_antigo" => "R$ 200,00",
+        "preco_novo" => "R$ 129,50",
+        "descricao" => "Zé Pilintra - Boneco Artesanal: Material: base em jornal, decorado com tecido. Acompanha caixa de presente branca com tampa transparente - acetato e fita."
+    ]
+];
+
+$produtos_biju = [
+    [
+        "nome" => "Anel Artesanal Olho de Hórus com Esfera de Pedra Natural",
+        "foto" => "../imagens/bijuterias/anel-artesanal-olho.jpg",
+        "preço" => "R$ 34,00",
+        "descricao" => "Anel artesanal de olho de Hórus com esfera de pedra natural, disponível em várias Pedras. Feito com Fio de Alpaca, Fio de Cobre ou Fio de Latão. Aberto atrás. Anel regulável feito sob medida."
+    ],
+    [
+        "nome" => "Bracelete ou Pulseira Artesanal de Arame com Pedras Naturais",
+        "foto" => "../imagens/bijuterias/bracelete-pulseira-artesanal.jpg",
+        "preço" => "R$ 79,00",
+        "descricao" => "Bracelete Artesanal trabalhado no latão com esferas de Ametista e Ágata Verde (pedras naturais). Material: Latão."
+    ],
+    [
+        "nome" => "Brinco Stars",
+        "foto" => "../imagens/bijuterias/brinco-stars-arte-indigena.jpg",
+        "preço" => "R$ 100,00",
+        "descricao" => "Arte Indígena , confeccionado de forma totalmente artesanal com miçangas. Fazemos sob medida e nas cores desejadas. Largura: 0.20 cm e Comprimento: 0.30 cm."
+    ],
+    [
+        "nome" => "Colar De Lápis Lazuli, Corrente E Pingente De Murano",
+        "foto" => "../imagens/bijuterias/colar-de-lapis-lazuli.jpg",
+        "preço" => "R$ 153,30",
+        "descricao" => "Colar de pedras com 45 cm de comprimento em lápis lazuli, detalhes em metal envernizado e magnífico pingente de murano com reflexos prata. Pingente 5X3cm."
+    ],
+    [
+        "nome" => "Gargantilha feito a mão",
+        "foto" => "../imagens/bijuterias/gargantilha-colar-em-ceramica-fria.jpg",
+        "preço" => "R$ 92,00",
+        "descricao" => "Colar, gargantilha em cerâmica fria. Nas cores branca, preto, amarelo e ouro negro. Cordão de algodão bege. Fecho em couro regulável. Comprimento 43 cm (regulável) e Pingente 7 cm."
+    ],
+    [
+        "nome" => "Pingente em Prata",
+        "foto" => "../imagens/bijuterias/pingente-em-prata-prata.jpg",
+        "preço" => "R$ 160,00",
+        "descricao" => "Pingente em prata esmaltada, feito a mão, tamanho aproximado 3 cm. Fazemos do tamanho desejado, personalizado. Peso: 10 g."
+    ]
+];
+
+$produtos_pinturas = [
+    [
+        "nome" => "Pesca Artesanal Recife - Pintura em Tela",
+        "foto" => "../imagens/pinturas/pesca-artesanal-em-tela.jpg",
+        "preço" => "R$ 2.300,00",
+        "descricao" => "QUADRO PESCA ARTESANAL: Pintura em tela técnica espatulado; Pintura com Tinta acrílica; Pintura feita à mão com riqueza de detalhes produzida pelo artista Fábio Santos; Sem moldura
+        MEDIDAS: 160 cm de comprimento, 80 cm de largura e 4 cm de altura."
+    ],
+    [
+        "nome" => "Bahia - Pintura em Tela",
+        "foto" => "../imagens/pinturas/bahia-pintura-em-tela.jpg",
+        "preço" => "R$ 4.500,00",
+        "descricao" => "QUADRO BAHIA: Pintura em tela técnica espatulado; Pintura com tinta acrílica; Pintura feita à mão com riqueza de detalhes produzida pelo artista Fábio Santos; Sem moldura. 
+        MEDIDAS: 160 cm de comprimento, 80 cm de largura e 4 cm de altura."
+    ],
+    [
+        "nome" => "Africana",
+        "foto" => "../imagens/pinturas/africana.jpg",
+        "preço" => "R$ 249,99",
+        "descricao" => "Tamanho 90x80cm; Pintura totalmente artesanal; Artista plástica: Daniela Tavares."
+    ],
+    [
+        "nome" => "Tela para Quadro - Mulata Afro Cores",
+        "foto" => "../imagens/pinturas/tela-para-quadro.jpg",
+        "preço" => "R$ 238,90",
+        "descricao" => "Tela composta de reprodução de pintura 50x70 cms impressa com qualidade FineArt diretamente em canvas importado de altíssima qualidade (tecido com qualidade Museu)."
+    ],
+    [
+        "nome" => "Quadros Afro - Baianos (Trio)",
+        "foto" => "../imagens/pinturas/quadros-afro-baianos-trio-salvador.jpg",
+        "preço" => "R$ 160,00",
+        "descricao" => "Trabalho artesanal, pintura toda feita à mão, com material da mais alta qualidade; Dupla camada protetora e seladora da pintura. Altura: 40.00 cm e Largura: 5.00 cm."
+    ],
+    [
+        "nome" => "Quadro Pintura Tela árvore contemporânea rosa",
+        "foto" => "../imagens/pinturas/quadro-pintura-tela-arvore-contemporanea-rosa.jpg",
+        "preço" => "R$ 790,00",
+        "descricao" => "Quadro de pintura em tela feito à mão usando acrílica ou óleo sobre tela. Indicado para qualquer ambiente."
+    ]
+];
